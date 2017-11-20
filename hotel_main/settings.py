42d37954a2fd0ba5251dc1a540678f04dfbb625e
RamirezJosue <xdemobtkx@gmail.com>
2017-11-19 20:55:58 -0600

    img

--- hotel_main/settings.py
+++ hotel_main/settings.py
@@ -87,13 +87,13 @@ WSGI_APPLICATION = 'hotel_main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
+DATABASESx = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASESx = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dnm7qls1ttsi5',
