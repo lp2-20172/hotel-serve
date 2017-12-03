134dd8f552d026f3e70838f4f0ea76aa055abed7
RamirezJosue <xdemobtkx@gmail.com>
2017-12-03 17:59:14 -0600

    sss

--- hotel/models/habitacion.py
+++ hotel/models/habitacion.py
@@ -9,9 +9,6 @@ class Habitacion(models.Model):
     piso = models.CharField(max_length=10)
     precio_diario = models.FloatField(default=0.0)
     tipo_habitacion = models.ForeignKey(Tipo_Habitacion, blank=True, null=True)
-    foto = models.ImageField(blank=True)
-    caracteristicas = models.TextField(null=True, blank=True)
-    descripcion = models.TextField(null=True, blank=True)
 
     class Meta:
         verbose_name = "Habitacion"
