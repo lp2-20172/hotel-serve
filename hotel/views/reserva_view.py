8f0f3f8b06776b2aa9c3b46a959ddd2a7d6bcd61
RamirezJosue <xdemobtkx@gmail.com>
2017-12-03 20:19:00 -0600

    nombre cliente

--- hotel/views/reserva_view.py
+++ hotel/views/reserva_view.py
@@ -1,16 +1,21 @@
 from ..models.reserva import Reserva
 from rest_framework import serializers, viewsets
+from rest_framework import permissions
 
 
 class ReservaSerializer(serializers.ModelSerializer):
+    nombre_cliente = serializers.SerializerMethodField()
 
     class Meta:
         model = Reserva
         fields = '__all__'
-        # fields = ('id', 'username', 'email', 'is_staff')
+
+    def get_nombre_cliente(self, obj):
+        return "%s %s %s" % (obj.cliente.nombre,
+                             obj.cliente.apellido_paterno,
+                             obj.cliente.apellido_materno)
 
 
 class ReservaViewSet(viewsets.ModelViewSet):
     queryset = Reserva.objects.all()
     serializer_class = ReservaSerializer
-    #permission_classes = [permissions.IsAuthenticated]
